e0460b14768f3afeba70a5183fdf7a1fd1123c8e
Kauane Chaves De Lima <kauane.lima@linx3.com>
2021-08-29 23:30:15 -0300

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-const express = require('express')
-const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+const express = require('express');
+const bodyParser = require ('body-parser')
+const app = express();
 
-app.get('/mensagens', function (req, res) {
-    res.send ('Exibir todas as mensagens.')
-})
- 
-app.listen(3000)
+app.use(bodyParser.json());
+
+express
+
+const port = 3000;
+
+const mensagens = [
+  "Primeira mensagem",
+  "Segunda mensagem"
+]
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+app.get('/mensagens', (req, res) => {
+    res.send(mensagens);
+  });
+
+  app.post('/mensagens', (req, res) => {
+    const mensagens = req.body.texto;
+
+    mensagens.push(mensagens);
+
+    res.send('mensagem criada com sucesso');
+  });
+
+app.listen(port, () => {
+  console.info('Servidor rodando em http://localhost:3000' + port);
+});
+
+//ler individual
+
+app.get('/mensaegns/:id', (req, res) => {
+  console.log(req.params);
+  res.send('Ler individual por ID');
+});
